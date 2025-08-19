bde8e8c122be4a088424e83a54784c1db04367e6
Sajal17 <sajalmondal1730@gmail.com>
2025-08-19 23:41:04 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -6,10 +6,11 @@ import java.sql.DriverManager;
 public class DBConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/college";
     private static final String USER = "root";
-    private static final String PASSWORD = "Sajal@1730"; // change!
+    private static final String PASSWORD = "Password"; // change
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
